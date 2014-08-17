60e7881cf7116f45e79ce5df95b929b91638dded
Lukas Reschke <lukas@owncloud.com>
2014-08-17 18:04:16 +0200

    Add routing to files_encryption

--- apps/files_encryption/appinfo/routes.php
+++ apps/files_encryption/appinfo/routes.php
@@ -5,5 +5,16 @@
  * See the COPYING-README file.
  */
 
+$this->create('ajax_adminrecovery', 'ajax/adminrecovery.php')
+	->actionInclude('files_encryption/ajax/adminrecovery.php');
+$this->create('ajax_changeRecoveryPassword', 'ajax/changeRecoveryPassword.php')
+	->actionInclude('files_encryption/ajax/changeRecoveryPassword.php');
+$this->create('ajax_getMigrationStatus', 'ajax/getMigrationStatus.php')
+	->actionInclude('files_encryption/ajax/getMigrationStatus.php');
+$this->create('ajax_updatePrivateKeyPassword', 'ajax/updatePrivateKeyPassword.php')
+	->actionInclude('files_encryption/ajax/updatePrivateKeyPassword.php');
+$this->create('ajax_userrecovery', 'ajax/userrecovery.php')
+	->actionInclude('files_encryption/ajax/userrecovery.php');
+
 // Register with the capabilities API
 OC_API::register('get', '/cloud/capabilities', array('OCA\Encryption\Capabilities', 'getCapabilities'), 'files_encryption', OC_API::USER_AUTH);
